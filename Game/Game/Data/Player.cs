3541a0f3c845db64823b99ad8f59c354af302e40
alex flick <macaronisupermoon@gmail.com>
2023-07-10 13:25:15 -0500

    added items to player too

--- Game/Game/Data/Player.cs
+++ Game/Game/Data/Player.cs
@@ -1,6 +1,7 @@
 ﻿using Game.Data.Amber;
 using Game.Data.Dinosaurs;
 using Game.Data.Fossils;
+using Game.Data.Items;
 
 namespace Game.Data;
 
@@ -15,4 +16,5 @@ public class Player
     public int AmberPerFossil { get; set; } = 3;
     public DinosaurEraEnum Era { get; set; } = DinosaurEraEnum.Triassic;
     public AmberType AmberType { get; set; } = AmberType.Yellow;
+    public List<Item> UnassignedItems { get; set; } = new List<Item>();
 }
